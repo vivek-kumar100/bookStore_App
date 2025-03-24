fd4b572308b28c4b400013620d21a6bc6390d438
vivek-kumar100 <vivek0451kumar@gmail.com>
2025-03-25 05:24:45 +0530

    Freebook Cards Complete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,15 @@
  import React from 'react';
  import Navbar from './components/Navbar';
  import Banner from './components/Banner';
+ import Freebook from './components/Freebook';
+ 
  
  function App() {
    return (<>
    <Navbar />
    <Banner />
+   <Freebook />
+
 
 
    </>
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
+ 
 
 function Navbar() {
+  const [sticky, setSticky] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const navItems =(
   <>
       <li><a>Home</a>
@@ -12,11 +29,18 @@ function Navbar() {
       <li><a>About</a>
       </li>
   
-  </>);
+  </>);  
   return (
   <>
-  <div  className={` max-w-screen-2xl container mx-auto md:px-20 px-4`}>
-  <div className="navbar bg-base-100">
+  
+  <div
+        className={` max-w-screen-2xl container mx-auto md:px-20 px-4 dark:bg-slate-800 dark:text-white fixed top-0 left-0 right-0 z-50 ${
+          sticky
+            ? "sticky-navbar shadow-md bg-base-200 dark:bg-slate-700 dark:text-white duration-300 transition-all ease-in-out"
+            : ""
+        }`}>
+
+  <div className="navbar">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
