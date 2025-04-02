a5bbd2439928141dc096124784e8c6f91a651cc1
vivek-kumar100 <vivek0451kumar@gmail.com>
2025-04-03 01:10:49 +0530

    Database Connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,31 @@
 import express from "express";
+import mongoose from 'mongoose';
 import dotenv from "dotenv";
+import bookRoute from "./route/book.route.js"
+
  
-const app = express()
-const PORT = process.env.PORT || 4003;
+const app = express();
+
 dotenv.config();
-app.get('/', (req, res) => {
-  res.send('Hello  vivek kumar !')
-})
+
+const PORT =process.env.PORT || 4000;
+const URI =process.env.MongoDBURI;
+
+// connect to mongoDB
+try {
+    mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log("Connected to mongoDB");
+} catch (error) {
+    console.log("Error: ", error);
+}
+
+// defining routes
+app.use("/book", bookRoute);
+ 
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
-})
+    console.log(`Server is listening on port ${PORT}`);
+});
